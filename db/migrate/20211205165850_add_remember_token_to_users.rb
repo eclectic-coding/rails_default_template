abe4d024dd3492cbd75830f9e10c4bea3cee6caa
eclecticcoding <chuck@eclecticsaddlebag.com>
2022-04-03 18:50:57 -0400

    Move out user autnetication4

--- db/migrate/20211205165850_add_remember_token_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddRememberTokenToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :remember_token, :string, null: false
-    add_index :users, :remember_token, unique: true
-  end
-end
--- db/migrate/20220201102359_add_request_columns_to_active_sessions.rb
+++
@@ -1,6 +0,0 @@
-class AddRequestColumnsToActiveSessions < ActiveRecord::Migration[6.1]
-  def change
-    add_column :active_sessions, :user_agent, :string
-    add_column :active_sessions, :ip_address, :string
-  end
-end
