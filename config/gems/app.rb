31209a9da3c4ed5e4ad1a26a749dc15b0fb7dd81
Chuck <chuck@eclecticsaddlebag.com>
2023-11-10 10:10:13 -0500

    Clean up template and remove rspec

--- config/gems/app.rb
+++ config/gems/app.rb
@@ -2,20 +2,12 @@ gem "bcrypt"
 
 group :development, :test do
   gem "standard", "~> 1.24", require: false
-  gem "capybara"
-  gem "webdrivers"
-  gem "rspec-rails", "~> 6.0.0"
-  gem "factory_bot_rails"
   gem "faker"
 end
 
 group :development do
-  gem "fuubar", "~> 2.5", ">= 2.5.1"
-  gem "guard"
-  gem "guard-rspec"
   gem "rubocop"
   gem "rubocop-rails", require: false
-  gem "rubocop-rspec"
 end
 
 group :test do
--- template.rb
+++ template.rb
@@ -17,6 +17,8 @@ end
 
 def add_javascript
   run "yarn add chokidar -D"
+
+  run "echo | node -v | cut -c 2- > .node-version"
 end
 
 def add_bootstrap
@@ -26,28 +28,10 @@ end
 def add_esbuild_script
   build_script = "node esbuild.config.mjs"
 
-  case `npx -v`.to_f
-  when 7.1...8.0
-    run %(npm set-script build "#{build_script}")
-    run %(yarn build)
-  when (8.0..)
-    run %(npm pkg set scripts.build="#{build_script}")
-    run %(yarn build)
-  else
-    say %(Add "scripts": { "build": "#{build_script}" } to your package.json), :green
-  end
+  say %(Add "scripts": { "build": "#{build_script}" } to your package.json), :green
 end
 
 def config_generators
-  # Jason Swett: "The Complete Guide to Rails Testing"
-  initializer "generators.rb", <<-CODE
-    Rails.application.config.generators do |g|
-      g.test_framework :rspec,
-        view_specs:       false,
-        helper_specs:     false,
-        routing_specs:    false
-    end
-  CODE
   inject_into_file "config/application.rb", "    config.generators.helper = false", after: "config.generators.system_tests = nil\n"
   inject_into_file "config/application.rb", "    config.generators.stylesheets = false\n\n", after: "config.generators.helper = false\n"
 end
@@ -60,20 +44,15 @@ end
 
 def copy_templates
   copy_file ".gitignore", force: true
-  copy_file ".rspec", force: true
   copy_file ".rubocop.yml"
   copy_file ".rubocop_rails.yml"
-  copy_file ".rubocop_rspec.yml"
   copy_file ".rubocop_strict.yml"
   copy_file ".rubocop_todo.yml"
-  copy_file ".simplecov"
   copy_file "esbuild.config.mjs"
-  copy_file "Guardfile"
   copy_file "Procfile.dev", force: true
 
   directory "app", force: true
   directory "lib", force: true
-  directory "spec", force: true
 
   environment "config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }", env: "development"
   environment "config.action_mailer.default_url_options = { host: 'example.com' }", env: "test"
@@ -85,7 +64,6 @@ def database_setup
 end
 
 def add_binstubs
-  run "bundle binstub rspec-core"
   run "bundle binstub rubocop"
 end
 
@@ -108,7 +86,7 @@ after_bundle do
   copy_templates
   add_esbuild_script
   config_generators
-  add_bootstrap
+  # add_bootstrap
   add_static
   database_setup
   add_binstubs
