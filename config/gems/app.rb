59d060fcc697e72f4b41113c2412ceff5ede51ce
Chuck Smith <csmit223@nd.edu>
2025-12-17 18:05:45 -0500

    Clean up gem file

--- config/gems/app.rb
+++ config/gems/app.rb
@@ -1,11 +1,8 @@
 gem "annotate"
-gem "bootstrap", "~> 5.3.3"
-gem "dartsass-rails"
 # gem "cssbundling-rails"
 gem "inline_svg"
 gem "name_of_person"
 # gem "strong_migrations" # Uncomment if you want to use strong_migrations
-gem "openssl", "~> 3.3", ">= 3.3.2"
 
 group :development, :test do
   gem "erb_lint"
@@ -14,6 +11,7 @@ end
 
 group :development do
   gem "bundle-audit", require: false
+  gem "hotwire-spark"
   # gem "bullet" # Uncomment if you want to use Bullet
   gem "letter_opener_web"
   gem "rails-erd"
